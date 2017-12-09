e7fe8644947af12861df0df60d48bff08c07baca
Dean Kerr <dean_k@live.co.uk>
2017-12-09 23:43:01 +0000

    remove stage 1 implementation

--- days/1/index.js
+++ days/1/index.js
@@ -1,11 +1,3 @@
-module.exports = (input, compareNextDigit) => (
-  input.split('')
-    .reduce((memo, value, i, a) => {
-      const toCompare = compareNextDigit ? a[i + 1] || a[0] : a[Math.floor(a.length / 2)];
-      return value === toCompare ? memo + Number(value) : memo;
-    }, 0)
-);
-
 module.exports = {
   nextDigit: input =>
     input.split('')
