57c2d65601a581fe6f2deb5d4b5c8343ed9c4e18
Dean Kerr <dean_k@live.co.uk>
2017-12-31 20:12:59 +0000

    add day 13 part two implementation

--- days/13/index.js
+++ days/13/index.js
@@ -10,19 +10,28 @@ const getHighestDepth = layers => layers.reduce((memo, layer) => (
   layer.depth > layers[0].depth ? layer.depth : layers[0].depth
 ), layers[0].depth);
 
-const performTrip = (delay, layers) => layers
-  .reduce((severity, { depth, range }) => {
-    const isCaught = (delay + depth) % ((2 * range) - 2) === 0;
-    return isCaught ? severity + (depth * range) : severity;
-  }, 0);
+const isCaught = (delay, range) => (delay % ((2 * range) - 2)) === 0;
 
-fs.readFile('./input2.txt', 'utf-8', (err, data) => {
+const calculateSeverity = (delay, layers) => layers
+  .filter(layer => isCaught(layer.depth, layer.range))
+  .reduce((severity, { depth, range }) => severity + (depth * range), 0);
+
+fs.readFile('./input.txt', 'utf-8', (err, data) => {
   if (err) console.error('error reading input file', err);
 
   const layers = parse(data);
   const highestDepth = getHighestDepth(layers);
-  const output = performTrip(0, layers);
+  const output = calculateSeverity(0, layers);
+
+  let delay = 0;
+  let isTripCaught = true;
+  while (isTripCaught) {
+    delay++;
+    // eslint-disable-next-line no-loop-func
+    isTripCaught = layers.some(layer => isCaught(delay + layer.depth, layer.range));
+  }
 
   console.log(`highest depth: ${highestDepth}`);
-  console.log(`trip severity: ${output}`);
+  console.log(`initial trip severity: ${output}`);
+  console.log(`delay to zero severity trip: ${delay}`);
 });
