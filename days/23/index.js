f53876502b138bf8e8ecc62e9d2c8790f3b6e556
Dean Kerr <dean_k@live.co.uk>
2018-01-05 23:46:54 +0000

    add day 23 part two

--- days/23/index.js
+++ days/23/index.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const isPrime = require('./is-prime');
+const findPrimes = require('./find-primes');
 
 const parse = require('./parse');
 
@@ -18,4 +20,22 @@ fs.readFile('./input.txt', 'utf-8', (err, data) => {
   }
 
   console.log(program.registry);
+
+  // Part two - count composite (aka non-prime) numbers between 108,400 and 125,000 in jumps of 17
+  const start = 108400;
+  const end = 125400;
+  const jump = 17;
+  const jumps = Math.ceil((end - start) / jump);
+  const sequence = [...Array(jumps + 1).keys()]
+    .map(value => start + (value * jump));
+
+  // Method One - isPrime check
+  console.log('prime check method', sequence.filter(value => !isPrime(value)).length);
+
+  // Method Two - Sieve of Eratosthenes
+  const primes = findPrimes(end)
+    .filter(value => value >= start)
+    .reduce((memo, number) => Object.assign({}, memo, { [number]: true }), {});
+
+  console.log('using sieve', sequence.filter(value => !primes[value]).length);
 });
